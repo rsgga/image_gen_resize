3eb9ebb588509e42ff5a440042f0e9808a5f602f
ryoichi-suganuma <ryoichi-suganuma@gmotech.jp>
2023-12-07 12:48:15 +0900

    update resize logic

--- image_resize.py
+++ image_resize.py
@@ -2,10 +2,52 @@ import sys
 import json
 from PIL import Image
 
+def get_resample_filter():
+    return Image.LANCZOS
+
+def calc_keep_aspect_new_size(original_size, output_size):
+    original_width, original_height = original_size
+    target_width,   target_height   = output_size
+
+    ratio = min(target_width/original_width, target_height/original_height)
+
+    return (int(original_width * ratio), int(original_height * ratio))
+
+def resize_image_keep_aspect_ratio(img, output_size, resample_filter):
+    new_size = calc_keep_aspect_new_size(img.size, output_size)
+    img = img.resize(new_size, resample_filter)
+
+    return img
+
+# for centering
+def top_left_point(img_size, canvas_size):
+    img_width, img_height = img_size
+
+    x = (canvas_size[0] - img_width) // 2
+    y = (canvas_size[1] - img_height) // 2
+
+    return (x, y)
+
+def center_image_on_canvas(img, canvas_size):
+    new_img = Image.new("RGB", canvas_size)
+
+    new_img.paste(
+        img,
+        top_left_point(img.size, canvas_size)
+    )
+
+    return new_img
+
+#def resize_image(input_path, output_path, output_size):
 def resize_image(input_path, output_path, new_width, new_height):
+    output_size = (new_width, new_height)
+
     with Image.open(input_path) as img:
-        img = img.resize((new_width, new_height))
-        img.save(output_path)
+        resample_filter = get_resample_filter()
+        resized_img = resize_image_keep_aspect_ratio(img, output_size, resample_filter)
+
+        adjusted_img = center_image_on_canvas(resized_img, output_size)
+        adjusted_img.save(output_path)
 
 def lambda_handler(event, context):
     body = json.loads(event['body'])
