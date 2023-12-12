cb5a1dc77bce4e853a1ef7527728d6817db62d97
ryoichi-suganuma <ryoichi-suganuma@gmotech.jp>
2023-12-12 12:26:33 +0900

    update prompt_translate.py and update README.md

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Image Generation and Resizing Script
-- This Python script is designed with AWS Lambda in mind.
+- A set of scripts for image generation, resizing, etc.
+- This Python script is designed with AWS Lambda in mind...
 
 ## image_generate.py
+`python3 ./image_generate.py {model} {prompt} {width} {height}`
 - Generates images using DALL-E.
 - The output is the URL of the generated image.
 - An OpenAI API key is required.
@@ -9,28 +11,31 @@
 ```
 $ pip3 install openai
 
-# python3 ./image_generate.py model prompt width height
 $ python3 ./image_generate.py "dall-e-2" "a girl, stencil art" "256" "256"
 $ python3 ./image_generate.py "dall-e-3" "a girl, stencil art" "256" "256"
 ```
 
 ## image_resize.py
+`python3 ./image_resize.py {input_path} {output_path} {width} {height}`
 - Resizes a given image.
 - The Pillow library is required.
 - Resizes while maintaining the aspect ratio.
     - This process involves creating a background with the size of the resized image and pasting the aspect ratio-adjusted image onto it.
 
+### Example
 ```
 $ pip3 install Pillow
 
-# python3 ./image_resize.py input_path output_path width height
 $ python3 ./image_resize.py "./${generated_file_name}" "./${resized_file_name}" "1164" "498"
 ```
 
 ## image_resolution.py
+`# python3 ./image_resolution.py {input_path} {output_path}`
 - Automatically adjusts the resolution of a given image.
 - Currently, many values are preset, so fine adjustments are not possible.
+- (Sorry, CUDA has not been tested...)
 
+### Example
 ```
 $ pip3 install Pillow
 $ pip3 install diffusers
@@ -38,19 +43,25 @@ $ pip3 install torch
 $ pip3 install transformers
 $ pip3 install accelerate
 
-# python3 ./image_resolution.py input_path output_path
 $ python3 ./image_resolution.py "./${generated_file_name}" "./${resized_file_name}"
 ```
 
 (Note: This translation covers the full content as per the snippet shown. If there is additional content, please let me know to continue the translation.)
 
 ## prompt_translate.py
+`# python3 ./prompt_translate.py {model} {source_text} {source_language} {target_language}`
 - Translates text passed as an argument into any language.
 - The source and target languages are assumed to be written in English.
+- An OpenAI API key is required.
 
+### Example
 ```
 $ pip3 install openai
-$ python3 ./prompt_translate.py "Welcome to the colorful and cute world of Toon Cats! A special piece for cat lovers. What's depicted here is a Toon Cat with vivid colors and a playful expression, as if it's jumped right out of an animation. This cat is not just a character. It's the protagonist of an adventure-filled story, always bringing a smile to our faces, a lovely presence." "Japanese" "English"
+
+# As of 2023/12/12, the first argument (openai model) that can be used in this script is as follows
+# - gpt-4-1106-preview
+# - gpt-3.5-turbo-1106
+$ python3 ./prompt_translate.py "gpt-4-1106-preview" "Welcome to the colorful and cute world of Toon Cats! A special piece for cat lovers. What's depicted here is a Toon Cat with vivid colors and a playful expression, as if it's jumped right out of an animation. This cat is not just a character. It's the protagonist of an adventure-filled story, always bringing a smile to our faces, a lovely presence." "Japanese" "English"
 ```
 
 ## sample.sh
--- prompt_translate.py
+++ prompt_translate.py
@@ -2,34 +2,28 @@ import sys
 import openai
 import json
 
+def system_prompt():
+    # It seems that in order for chatgpt to output json, content that outputs json is required.
+    # ref. https://platform.openai.com/docs/guides/text-generation/json-mode
+    return "You are a language translation model to output JSON."
 
-def translate_text(source_text, source_language, target_language):
-    """
-    Translates the given text from the source language to the target language using OpenAI's translation model.
+def translation_prompt(source_text, source_language, target_language):
+    return f"Translate the following text from {source_language} to {target_language}:\n\n{source_text}"
 
-    Parameters:
-    source_text (str): The text to be translated.
-    source_language (str): The language of the source text.
-    target_language (str): The language to translate the text into.
-
-    Returns:
-    str: The translated text.
-    """
-
-    translation_prompt = f"Translate the following text from {source_language} to {target_language}:\n\n{source_text}"
-
-    # FIXME: I want to get model from the argument
+def translate_text(openai_model, source_text, source_language, target_language):
+    # As of 12/12/2023, only the following models are available
+    # - gpt-4-1106-preview
+    # - gpt-3.5-turbo-1106
+    # ref. https://platform.openai.com/docs/guides/text-generation/json-mode
     response = openai.chat.completions.create(
-        model="gpt-3.5-turbo-1106",
+        model=openai_model,
         response_format={ "type": "json_object" },
         messages=[
-            {"role": "system", "content": "You are a language translation model to output JSON."},
-            {"role": "user", "content": translation_prompt}
+            {"role": "system", "content": system_prompt()},
+            {"role": "user",   "content": translation_prompt(source_text, source_language, target_language)}
         ]
     )
 
-
-    #print(response.choices[0].message.content)
     loaded_obj = json.loads(response.choices[0].message.content)
 
     values_list = list(loaded_obj.values())
@@ -40,31 +34,30 @@ def translate_text(source_text, source_language, target_language):
 def lambda_handler(event, context):
     body = json.loads(event['body'])
 
-    prompt      = body['prompt']
-    source_lang = body['source_lang']
-    target_lang = body['target_lang']
+    openai_model = body['openai_model']
+    prompt       = body['prompt']
+    source_lang  = body['source_lang']
+    target_lang  = body['target_lang']
 
-    translated_text = translate_text(prompt, source_lang, target_lang)
+    translated_text = translate_text(openai_model, prompt, source_lang, target_lang)
 
     print(translated_text)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python prompt_translate_japanese_to_english.py <prompt> <source_language_in_english> <target_language_in_english>")
+    if len(sys.argv) != 5:
+        print("Usage: python prompt_translate_japanese_to_english.py <openai_model> <prompt> <source_language_in_english> <target_language_in_english>")
         sys.exit(1)
 
-    prompt      = sys.argv[1]
-    source_lang = sys.argv[2]
-    target_lang = sys.argv[3]
-
-    # Fixed value
-    quality = "standard"
+    openai_model = sys.argv[1]
+    prompt       = sys.argv[2]
+    source_lang  = sys.argv[3]
+    target_lang  = sys.argv[4]
 
     json_body = json.dumps({
-        "prompt":  prompt,
-        "source_lang": source_lang,
-        "target_lang": target_lang
+        "openai_model": openai_model,
+        "prompt":       prompt,
+        "source_lang":  source_lang,
+        "target_lang":  target_lang
     })
 
     lambda_handler({ "body": json_body }, {})
-
